926ed2f8aff86e0d02625b71b968d6fe907fad7c
matildehomem <matilde.a.s.homem@gmail.com>
2019-01-27 14:36:05 +0000

    gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import SEO from '../components/seo'
 import { HomeHeader, Banner, BannerButton } from '../utils'
 import img from '../images/bcg/homeBcg.jpg'
 import QuickInfo from '../components/HomePageComponents/QuickInfo';
+import Gallery from '../components/HomePageComponents/Gallery-1';
 
 const IndexPage = () => (
   <Layout>
@@ -15,6 +16,7 @@ const IndexPage = () => (
       </Banner>
     </HomeHeader>
     <QuickInfo></QuickInfo>
+    <Gallery />
   </Layout>
 )
 
